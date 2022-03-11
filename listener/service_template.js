51e107a45cec7ed7e4f812660f960b59d7b249f6
Yanux <yanux@yanux.info>
2022-03-11 16:50:48 +0100

    Fix tosca object acces

--- listener/service_template.js
+++ listener/service_template.js
@@ -4,7 +4,7 @@ export default {
         let cst = parsed_rule.ctx.prog.current_service_template
 
         cst.tosca_definitions_version = val.tosca_definitions_version?.value
-        cst.description = val.description?.value.tosca
-        cst.metadata = val.metadata?.value.tosca
+        cst.description = val.description?.tosca
+        cst.metadata = val.metadata?.tosca
     }
 }
