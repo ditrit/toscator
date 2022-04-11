ee733c3ada47ed9e2d303813e6d6780381fc7061
achraftha <thabet.achraf@yahoo.com>
2022-04-11 10:45:28 +0200

    fix Path for OS

--- model/Charac.js
+++ model/Charac.js
@@ -1,49 +1,49 @@
-import {
-    ToscaMetadata
-} from "./metadata.js"
-import {
-    DefType
-} from "./def_type.js"
-import {
-    ToscaNodeType
-} from "./node_type.js"
+import { ToscaMetadata } from "./metadata.js";
+import { DefType } from "./def_type.js";
+import { ToscaNodeType } from "./node_type.js";
 
-export class Charac extends ToscaNodeType {
-    constructor(input, source) {
-        super(input, source)
-        this.type = input.type
-        this.default = input.default
-        this.status = input.status
-        this.metadata = input.metadata
-        this.name = input.name
-    }
-    static statusValues = ['supported', 'unsupported', 'experimental', 'deprecated', 'optional'];
+export class Charac extends DefType {
+   constructor(input, source) {
+      super(input, source);
+      this.type = input.type;
+      this.default = input.default;
+      this.status = input.status;
+      this.metadata = input.metadata;
+      this.name = input.name;
+   }
+   static statusValues = [
+      "supported",
+      "unsupported",
+      "experimental",
+      "deprecated",
+      "optional",
+   ];
 
-    static isValid(input) {
-        let res = true
-        if (!super.isValid(input)) {
-            res = false
-        }
-        // if (input.default && !input.default instanceof ToscaValue) { // TODO
-        //     res=false
-        // }
-        if (input.status && !input.status instanceof String) {
-            res = false
-        } else if (!input.status in this.statusValues) {
-            res = false
-        }
-        if (input.metadata && !input.metadata instanceof ToscaMetadata) {
-            res = false
-        }
-        if (input.name && !input.name instanceof String) {
-            res = false
-        }
-        return res
-    }
+   static isValid(input) {
+      let res = true;
+      if (!super.isValid(input)) {
+         res = false;
+      }
+      // if (input.default && !input.default instanceof ToscaValue) { // TODO
+      //     res=false
+      // }
+      if (input.status && !input.status instanceof String) {
+         res = false;
+      } else if (!input.status in this.statusValues) {
+         res = false;
+      }
+      if (input.metadata && !input.metadata instanceof ToscaMetadata) {
+         res = false;
+      }
+      if (input.name && !input.name instanceof String) {
+         res = false;
+      }
+      return res;
+   }
 }
 
 export function newCharac(input, source) {
-    let res
-    Charac.isValid(input) ? res = newCharac(input, source) : res = {}
-    return res
+   let res;
+   Charac.isValid(input) ? (res = newCharac(input, source)) : (res = {});
+   return res;
 }
--- model/imports.js
+++ model/imports.js
@@ -1,79 +1,82 @@
-import {
-    ToscaNode
-} from './tosca_node.js'
-import path from 'path'
-
+import { ToscaNode } from "./tosca_node.js";
+import path from "path";
 
 export class ToscaImport extends ToscaNode {
-    constructor(input, source) {
-        super(source)
-        this.file = input.file
-        this.currentPath = (input.currentPath) ? input.currentPath : "."
-        this.repository = input.repository
-        this.namespace_prefix = input.namespace_prefix
-        this.namespace_uri = input.namespace_uri
-        this.setAbsolutePath()
-    }
+   constructor(input, source) {
+      super(source);
+      this.file = input.file;
+      this.currentPath = input.currentPath ? input.currentPath : ".";
+      this.repository = input.repository;
+      this.namespace_prefix = input.namespace_prefix;
+      this.namespace_uri = input.namespace_uri;
+      this.setAbsolutePath();
+   }
 
+   toString() {
+      return `\n    {Path: ${this.path}, \n    Repository: ${this.repository}, \n    Namespace_prefix: ${this.namespace_prefix}, \n    Namespace_uri: ${this.namespace_uri}}\n`;
+   }
 
-    toString() {
-        return `\n    {Path: ${this.path}, \n    Repository: ${this.repository}, \n    Namespace_prefix: ${this.namespace_prefix}, \n    Namespace_uri: ${this.namespace_uri}}\n`
-    }
+   static isValid(input, source) {
+      if (typeof input.file != "string" || input.file == "") {
+         source.ctx.grammarError("Incorrect file input for import");
+         return false;
+      }
+      if (
+         typeof input.repository != "string" ||
+         typeof input.namespace_prefix != "string" ||
+         typeof input.namespace_uri != "string"
+      ) {
+         source.ctx.grammarError("Incorrect file input for import");
+         return false;
+      }
+      return true;
+   }
 
+   setAbsolutePath() {
+      // ##### TODO: MANAGE REPOSITORIES #####
+      this.path = "";
+      if (
+         this.repository &&
+         this.repository != "" &&
+         this.isRelative(this.file)
+      ) {
+         this.path = `${this.source.ctx.prog.repositories[
+            this.repository
+         ].getFullUrl()}/${this.file}`;
+      } else {
+         this.path = this.isRelative(this.file)
+            ? path.join(this.currentPath, this.file)
+            : this.file;
+      }
+      if (!this.path.match(/^[a-zA-Z]*:\/\//)) {
+         this.path = path.resolve(this.path);
+      } else {
+         this.path = path.resolve("/" + this.path).substring(1);
+      }
+      this.currentPath = path.dirname(this.path);
+      this.source.ctx.prog.currentPath = this.currentPath;
+   }
 
-    static isValid(input, source) {
-        if (typeof(input.file) != 'string' || input.file == "") {
-            source.ctx.grammarError('Incorrect file input for import')
-            return false
-        }
-        if (typeof(input.repository) != 'string' ||
-            typeof(input.namespace_prefix) != 'string' ||
-            typeof(input.namespace_uri) != 'string') {
+   isRelative(path) {
+      return !path.match(/^[a-zA-Z]*:\/\//) || !path[0] == "/";
+   }
 
-            source.ctx.grammarError('Incorrect file input for import')
-            return false
-        }
-        return true
-    }
-
-
-    setAbsolutePath() {
-        // ##### TODO: MANAGE REPOSITORIES #####
-        this.path = ""
-        if (this.repository && this.repository != "" && this.isRelative(this.file)) {
-            this.path = `${this.source.ctx.prog.repositories[this.repository].getFullUrl()}/${this.file}`
-        } else {
-
-            this.path = (this.isRelative(this.file)) ?
-                this.currentPath + '/' + this.file :
-                this.file
-        }
-        if (!this.path.match(/^[a-zA-Z]*:\/\//)) {
-            this.path = path.resolve(this.path)
-        } else {
-            this.path = path.resolve("/" + this.path).substring(1)
-        }
-        this.currentPath = path.dirname(this.path)
-        this.source.ctx.prog.currentPath = this.currentPath
-    }
-
-    isRelative(path) {
-        return (!path.match(/^[a-zA-Z]*:\/\//) || !path[0] == '/')
-    }
-
-    equals(other) {
-        return (other instanceof ToscaImport && this.path == other.path && this.namespace_prefix == other.namespace_prefix && this.namespace_uri == other.namespace_uri)
-    }
+   equals(other) {
+      return (
+         other instanceof ToscaImport &&
+         this.path == other.path &&
+         this.namespace_prefix == other.namespace_prefix &&
+         this.namespace_uri == other.namespace_uri
+      );
+   }
 }
 
-
-
 export function newToscaImport(input, source) {
-    let res
-    if (ToscaImport.isValid(input, source)) {
-        res = new ToscaImport(input, source)
-    } else {
-        res = {}
-    }
-    return res
+   let res;
+   if (ToscaImport.isValid(input, source)) {
+      res = new ToscaImport(input, source);
+   } else {
+      res = {};
+   }
+   return res;
 }
