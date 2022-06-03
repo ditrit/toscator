22fdd95380fdc6bae4d5643ead0e5d293b35a849
AldanTanneo <aldantanneo@gmail.com>
2022-06-03 16:34:51 +0200

    fix operation_def

--- listener/operation_def.js
+++ listener/operation_def.js
@@ -1,14 +1,14 @@
 import { newOperationDef } from "../model/operation_def.js";
 
 export default {
-   exit_operation_def(parsed_rule) {
-      newOperationDef(
-         {
-            description: parsed_rule.value.description?.value,
-            inputs: parsed_rule.value.inputs?.value,
-            implementation: parsed_rule.value.implementation?.value,
-         },
-         parsed_rule
-      );
-   },
+    exit_operation_def(parsed_rule) {
+        newOperationDef(
+            {
+                description: parsed_rule.value.description?.value,
+                inputs: parsed_rule.value.inputs?.value,
+                implementation: parsed_rule.value.implementation?.tosca,
+            },
+            parsed_rule
+        );
+    },
 };
