82e6098b5ea5d74ec571495a9dfe65ac0aea6da3
achraftha <thabet.achraf@yahoo.com>
2022-06-07 10:21:46 +0200

    Update Group type

--- listener/group_type.js
+++ listener/group_type.js
@@ -1,24 +1,47 @@
 import { newToscaGroupType } from "../model/group_type.js";
 
 export default {
-    exit_group_types(parsed_rule) {
-        for (const key in parsed_rule.value) {
-            parsed_rule.value[key].tosca.setId(key, parsed_rule, "group_types")
-        }
-    },
+   exit_group_types(parsed_rule) {
+      for (const key in parsed_rule.value) {
+         parsed_rule.value[key].tosca?.setId(key, parsed_rule, "group_types");
+      }
+   },
 
-    exit_group_type(parsed_rule) {
-        let properties = new Map();
-        for (const key in parsed_rule.value.properties.value) {
-            properties[key] = parsed_rule.value.properties.value[key].tosca
-        }
-        newToscaGroupType({
+   exit_group_type(parsed_rule) {
+      let properties = new Map();
+      let capabilities = new Map();
+      let requirements = new Map();
+      let interfaces = new Map();
+      let members = [];
+      for (const key in parsed_rule.value.properties?.value) {
+         properties[key] = parsed_rule.value.properties.value[key].tosca;
+      }
+      for (const key in parsed_rule.value.capabilities?.value) {
+         capabilities[key] = parsed_rule.value.capabilities.value[key].tosca;
+      }
+      for (const key in parsed_rule.value.requirements?.value) {
+         requirements[key] = parsed_rule.value.requirements.value[key].tosca;
+      }
+      for (const key in parsed_rule.value.interfaces?.value) {
+         interfaces[key] = parsed_rule.value.interfaces.value[key].tosca;
+      }
+      for (const key in parsed_rule.value.members?.value) {
+         members.push(parsed_rule.value.members.value[key].value);
+      }
+
+      newToscaGroupType(
+         {
             derived_from: parsed_rule.value.derived_from?.value,
             version: parsed_rule.value.version?.tosca,
-            description: parsed_rule.value.description?.tosca,
+            description: parsed_rule.value.description?.value,
             metadata: parsed_rule.value.metadata?.tosca,
             properties: properties,
-        }, 
-            parsed_rule)
-    }
-}
+            capabilities: capabilities,
+            requirements: requirements,
+            interfaces: interfaces,
+            members: members,
+         },
+         parsed_rule
+      );
+   },
+};
--- model/group_type.js
+++ model/group_type.js
@@ -1,35 +1,42 @@
-import { ToscaType } from "./tosca_type.js"
+import { ToscaType } from "./tosca_type.js";
 
 export class ToscaGroupType extends ToscaType {
-    constructor(input, source) {
-        super(input, source)
-    }
+   constructor(input, source) {
+      super(input, source);
+      this.properties = input.properties;
+      this.capabilities = input.capabilities;
+      this.requirements = input.requirements;
+      this.interfaces = input.interfaces;
+      this.members = input.members;
+   }
 
-    static _classname = 'group_type'
-    
-    getClassname() {
-        return ToscaGroupType._classname
-    }
+   static _classname = "group_type";
 
-    toString() {
-        return super.toString()
-    }
-    static isValid(input, source) {
-        if(!ToscaType.isValid(input, source)) {
-            
-            source.ctx.typeError(source.current, 'Incorrect definition for GroupType')
-            return false
-        }
-        return true
-    }
+   getClassname() {
+      return ToscaGroupType._classname;
+   }
+
+   toString() {
+      return super.toString();
+   }
+   static isValid(input, source) {
+      if (!super.isValid(input, source)) {
+         source.ctx.typeError(
+            source.current,
+            "Incorrect definition for GroupType"
+         );
+         return false;
+      }
+      return true;
+   }
 }
 
 export function newToscaGroupType(input, source) {
-    let res
-    if (ToscaGroupType.isValid(input, source)) {
-        res = new ToscaGroupType(input, source)
-    } else {
-        res = {}
-    }
-    return res
+   let res;
+   if (ToscaGroupType.isValid(input, source)) {
+      res = new ToscaGroupType(input, source);
+   } else {
+      res = {};
+   }
+   return res;
 }
