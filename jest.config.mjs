a98b4a278e5c9a970331296c99e7dcac75198699
achraftha <thabet.achraf@yahoo.com>
2022-06-14 09:52:12 +0200

    Setup jest on Toscator

---
+++ jest.config.mjs
@@ -0,0 +1,28 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+   globals: {
+      __DEV__: true,
+   },
+   // Jest assumes we are testing in node environment, specify jsdom environment instead
+   testEnvironment: "node",
+   // Needed in JS codebases too because of feature flags
+   coveragePathIgnorePatterns: ["/node_modules/", ".d.ts$"],
+   testMatch: ["<rootDir>/tests/unit/**/*.spec.js"],
+   moduleFileExtensions: ["js"],
+   moduleNameMapper: {
+      "^src/(.*)$": "<rootDir>/src/$1",
+   },
+   // transform: {
+   //   ".*\\.js$": "babel-jest",
+   // },
+   transformIgnorePatterns: ["<rootDir>/node_modules/"],
+   // testResultsProcessor: "jest-sonar-reporter",
+   collectCoverage: true,
+   collectCoverageFrom: ["src/**/*.js"],
+   coverageReporters: ["lcov", "cobertura", "text-summary"],
+   coverageDirectory: "./reports",
+};
