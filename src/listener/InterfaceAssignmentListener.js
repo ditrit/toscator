e0d91ad1db6d03ec3b1b294d886af384ab06adda
achraftha <thabet.achraf@yahoo.com>
2022-07-01 14:19:50 +0200

    Create interface assignment

---
+++ src/listener/InterfaceAssignmentListener.js
@@ -0,0 +1,11 @@
+export default {
+   exit_interface_assignments(parsed_rule) {
+      console.log("exit_interface_assignments");
+
+      //   parsed_rule.tosca = parsed_rule.value;
+   },
+
+   exit_interface_assignments(parsed_rule) {
+      console.log("exit_interface_assignments");
+   },
+};
