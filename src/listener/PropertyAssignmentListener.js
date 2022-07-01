18d9d97cf9927af28576257c51603c63d1146ed9
achraftha <thabet.achraf@yahoo.com>
2022-07-01 14:21:53 +0200

    attribute and input assignments

--- src/listener/PropertyAssignmentListener.js
+++ src/listener/PropertyAssignmentListener.js
@@ -2,4 +2,11 @@ export default {
    exit_property_assignments(parsed_rule) {
       parsed_rule.tosca = parsed_rule.value;
    },
+
+   exit_attribute_assignments(parsed_rule) {
+      parsed_rule.tosca = parsed_rule.value;
+   },
+   exit_input_assignments(parsed_rule) {
+      parsed_rule.tosca = parsed_rule.value;
+   },
 };
