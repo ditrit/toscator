00099da8980c49a3922c44446f93a4e927feec50
Thibaud Labat <thibaud.labat@hotmail.fr>
2023-08-24 16:17:45 +0200

    eslint rules

--- src/listener/listener_helpers/listener_helpers.js
+++ src/listener/listener_helpers/listener_helpers.js
@@ -1,12 +1,12 @@
-import {defListofHelper} from './def_listof_helper.js';
-import {defMapofHelperSetname, defPropertyMapofHelper} from './def_mapof_helper.js';
-import {propertyMapofHelper} from './property_mapof_helper.js';
-import {propertyListofHelper} from './property_listof_helper.js';
+import { defListofHelper } from './def_listof_helper.js';
+import { defMapofHelperSetname, defPropertyMapofHelper } from './def_mapof_helper.js';
+import { propertyMapofHelper } from './property_mapof_helper.js';
+import { propertyListofHelper } from './property_listof_helper.js';
 
 export default {
-    defPropertyMapofHelper,
-    defListofHelper,
-    defMapofHelperSetname,
-    propertyMapofHelper,
-    propertyListofHelper
+  defPropertyMapofHelper,
+  defListofHelper,
+  defMapofHelperSetname,
+  propertyMapofHelper,
+  propertyListofHelper,
 };
