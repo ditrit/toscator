c0306bf329187979a908f7555da9594222e8c0a1
Thibaud Labat <thibaud.labat@hotmail.fr>
2023-08-30 17:04:53 +0200

    Jest setup

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    globals: {
+        __DEV__: true,
+    },
+    // Jest assumes we are testing in node environment, specify jsdom environment instead
+    testEnvironment: 'jest-environment-node',
+    // Needed in JS codebases too because of feature flags
+    coveragePathIgnorePatterns: ['/node_modules/', '.d.ts$'],
+    testMatch: [
+        '<rootDir>/tests/unit/**/*.spec.js',
+    ],
+    moduleFileExtensions: ['js'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^tests/(.*)$': '<rootDir>/tests/$1',
+        '^package\\.json$': '<rootDir>/package.json',
+    },
+    transform: {
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)'],
+    //testResultsProcessor: 'jest-sonar-reporter',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.js'],
+    coverageReporters: ['lcov', 'cobertura', 'text-summary', 'text'],
+    coverageDirectory: './reports',
+};
---
+++ tests/unit/parse.spec.js
@@ -0,0 +1,24 @@
+import {parse} from "../../src/parser/parse.js";
+import {describe, it, expect} from "@jest/globals";
+import parserJson from "./parse.json";
+describe('Parsing', ()=>{
+    it('gives a reproducible output',()=>{
+        // provide the relative path to the tosca file to parse
+        const res = parse('tests/data_manual/test_parsing/testInterfaceMapping.yml');
+
+        const seen = new Set();
+        function replacer(key,value)
+        {
+            if(key==="origin_file")
+                return "__IGNORED__"; // ignore
+
+            // ignore circular references
+            if(seen.has(value))
+                return;
+            seen.add(value);
+
+            return value;
+        }
+        expect(JSON.parse(JSON.stringify(res, replacer))).toEqual(parserJson);
+    });
+});
