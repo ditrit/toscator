6b1afe72e0ecebb4892f3e7cc9f5d4539db44925
Thibaud Labat <thibaud.labat@hotmail.fr>
2023-08-30 18:17:19 +0200

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import { parse } from './parser/parse.js'
-export { parse };
+import { RuleParser } from 'lidy-js/parser/ruleparser.js';
+import { compile } from '#src/compilation.js';
 
+RuleParser.throwOnError = true;
+// const result = compile('tests/data_manual/ToscaNextcloud.yml');
+const result = compile('tests/unit/types_inheritance/node_type_derived_from.yml');
+console.log(result);
+console.log(result.node_types);
+console.log(result.node_types.get('test_parent_type'));
