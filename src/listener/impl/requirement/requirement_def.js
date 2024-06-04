56aad3778b38d569efc181a18a4cd15e6ef218ad
Rahzbob <tom.dejardin94@gmail.com>
2024-06-04 15:29:26 +0200

    Update requirement

--- src/listener/impl/requirement/requirement_def.js
+++ src/listener/impl/requirement/requirement_def.js
@@ -6,7 +6,6 @@ export default {
     const requirements = [];
     for (const key in parsed_rule.value) {
       const requirement_name = parsed_rule.value[key].tosca.name;
-      delete parsed_rule.value[key].tosca.name;
       requirements.push({
         [requirement_name]: parsed_rule.value[key].tosca,
       });
@@ -30,7 +29,7 @@ export default {
             capability: parsed_rule.value[key].value.capability?.value,
             node: parsed_rule.value[key].value.node?.value,
             relationship:
-                     parsed_rule.value[key].value.relationship?.tosca,
+            parsed_rule.value[key].value.relationship?.tosca,
             occurences: parsed_rule.value[key].value.occurrences?.value,
             name: key,
           },
